c7346d0446c0544a41177c770389dbe71fa1d6f6
dms218 <dms218@lehigh.edu>
2015-08-30 17:39:18 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,28 @@
+//Dean Shafritz
+//Welcome Class Java Program
+//
+//  first compile the program
+//      javac HelloWorld.java
+//  run the program
+//      java HelloWorld//
+
+//  define a class
+public class WelcomeClass{
+    
+//add main method
+    public static void main(String[] args) {
+
+//print the statement 
+    System.out.println("-----------");
+    System.out.println("| WELCOME |");
+    System.out.println("-----------");
+    System.out.println(" ^  ^  ^  ^  ^  ^");
+    System.out.println("/ \\/ \\/ \\/ \\/ \\/ \\ ");
+    System.out.println("|-D--M--S--2--1--8-|");
+    System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+    System.out.println("  v  v  v  v  v  v");
+    System.out.println("My name is Dean Shafritz and i work as a beach");
+    System.out.println("lifeguard in the summer.");
+    //test/testtest
+    }
+}
